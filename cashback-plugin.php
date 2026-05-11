2d3c88ea7f930d80b8fc290be9d4a33b3ed33c26
Igor-creato <71209783+Igor-creato@users.noreply.github.com>
2026-05-11 18:11:59 +0300

    Версия 4.0.0

--- cashback-plugin.php
+++ cashback-plugin.php
@@ -6,7 +6,7 @@ declare(strict_types=1);
 /**
  * Plugin Name: Cashback Plugin
  * Description: Объединенный плагин для системы кэшбэка и аффилиат-партнерства
- * Version: 3.1.0
+ * Version: 4.0.0
  * Author: Cashback
  * Author URI: https://example.com
  * Text Domain: cashback-plugin
